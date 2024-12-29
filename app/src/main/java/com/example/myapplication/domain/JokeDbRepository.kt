d34fc0247d8ed3a2f433a1995c67ad23188c8cdb
mareliberum <vladfokin646@gmail.com>
2024-12-29 12:48:20 +0300

    merge 13 to main

--- app/src/main/java/com/example/myapplication/domain/JokeDbRepository.kt
+++ app/src/main/java/com/example/myapplication/domain/JokeDbRepository.kt
@@ -2,18 +2,11 @@ package com.example.myapplication.domain
 
 import com.example.myapplication.data.db.Joke
 
-//TODO : уменьшить интерфейс и унаследовать от него 2 JokesRepository
 interface JokeDbRepository {
-    //методы для сетевого кэша
     suspend fun refreshJokes() : Boolean
-    suspend fun getJokesFromApiDatabase(): List<Joke>
+    suspend fun getAllJokes(): List<Joke>
     suspend fun deleteExpired(expirationTime : Long)
     suspend fun clearDb()
-    suspend fun addJokeToLocalDatabase(joke: Joke)
-    suspend fun deleteJokeFromApiDatabase(id: Int)
-
-    //методы для бд с локальными шуткми
-    suspend fun deleteJokeFromLoacalDatabaae(id: Int)
-    suspend fun loadStaticJokes()
-    suspend fun getLoacalJokes() : List<Joke>
+    suspend fun addJoke(joke: Joke)
+    suspend fun deleteJoke(id: Int)
 }
